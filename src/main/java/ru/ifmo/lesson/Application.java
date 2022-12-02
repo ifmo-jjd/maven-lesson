2352783fa9c814bb3dd7c990f7daa504a4fd4b59
Dasha <byrtosha@yandex.ru>
2022-12-02 22:43:06 +0300

    Занятие 31: maven

---
+++ src/main/java/ru/ifmo/lesson/Application.java
@@ -0,0 +1,8 @@
+package ru.ifmo.lesson;
+
+public class Application {
+    public static void main(String[] args) {
+        Cat cat = new Cat();
+        System.out.println(cat.getAge());
+    }
+}
---
+++ src/main/java/ru/ifmo/lesson/Cat.java
@@ -0,0 +1,12 @@
+package ru.ifmo.lesson;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Getter
+@ToString
+public class Cat {
+    private String name;
+    private String color;
+    private int age;
+}
